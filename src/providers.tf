5b1a3bfd0025e72057ed6dca7d555fcf7b394d91
= <=>
2024-01-01 09:17:52 +0000

    cicd 00

--- src/providers.tf
+++ src/providers.tf
@@ -1,9 +1,34 @@
-terraform {
+# terraform {
 
+#   backend "s3" {
+#     #bucket exists already / change to an existing bucket
+#     bucket = "sedem-terra333-bucket"
+#     key    = "bid-project/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.31.0"
+#     }
+#   }
+# }
+
+# provider "aws" {
+#   region = "us-east-1"
+#   alias  = "bid_region"
+# }
+
+
+
+
+terraform {
+  #comment the backend config below, to run the code in your local environment
   backend "s3" {
-    #bucket exists already / change to an existing bucket
-    bucket = "tproject1"
-    key    = "bid-project/terraform.tfstate"
+    #bucket exists already
+    bucket = "sedem-terra333-bucket"
+    key    = "terraform-project-jomacs-/terraform.tfstate"
     region = "us-east-1"
   }
 
@@ -17,32 +42,8 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  alias  = "bid_region"
+  alias  = "project_region"
 }
-
-
-
-
-# terraform {
-#   #comment the backend config below, to run the code in your local environment
-#   # backend "s3" {
-#   #   #bucket exists already
-#   #   bucket = "sedem-terra333-bucket"
-#   #   key    = "terraform-project-jomacs-/terraform.tfstate"
-#   #   region = "us-east-1"
-#   # }
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.17.0"
-#     }
-#   }
-# }
-
-# provider "aws" {
-#   region = "us-east-1"
-#   alias  = "project_region"
-# }
 
 
 
