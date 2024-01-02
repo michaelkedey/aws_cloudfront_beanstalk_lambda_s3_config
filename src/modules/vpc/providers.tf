677b04a4f8a476e7ae509e20aab39f3f3c38c2c7
= <=>
2024-01-02 20:42:55 +0000

    cicd-000-00778

---
+++ src/modules/vpc/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.17.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "bid_region"
+}
+
+
