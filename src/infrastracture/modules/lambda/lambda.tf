b9af884cbc91107fa0d08a6c7911cedb6f49df1f
= <=>
2024-01-06 16:17:23 +0000

    o_o

--- src/infrastracture/modules/lambda/lambda.tf
+++ src/infrastracture/modules/lambda/lambda.tf
@@ -15,18 +15,12 @@ resource "aws_iam_role" "lambda_execution_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_execution_role_policy_attachment" {
-  policy_arn = var.lambda_policy_arn
-  role       = aws_iam_role.lambda_execution_role.name
-}
-
 
 resource "aws_iam_policy" "lambda_execution_policy" {
   name        = "LambdaExecutionPolicy"
   description = "Policy for Lambda execution"
   policy      = file("${path.module}/lambda_execution_policy.json")
 }
-
 
 resource "aws_iam_role_policy_attachment" "lambda_execution_role_policy_attachment-2" {
   policy_arn = aws_iam_policy.lambda_execution_policy.arn
@@ -111,32 +105,6 @@ resource "aws_s3_bucket_notification" "lambda_trigger" {
 
   depends_on = [aws_lambda_permission.allow_bucket]
 }
-
-
-
-# resource "aws_s3_bucket_notification" "bucket_notification" {
-#   bucket = aws_s3_bucket.bucket.id
-
-#   lambda_function {
-#     lambda_function_arn = aws_lambda_function.func.arn
-#     events              = ["s3:ObjectCreated:*"]
-#     filter_prefix       = "AWSLogs/"
-#     filter_suffix       = ".log"
-#   }
-
-#   depends_on = [aws_lambda_permission.allow_bucket]
-# }
-
-
-
-
-# resource "aws_lambda_permission" "allow_bucket" {
-#   statement_id  = "AllowExecutionFromS3Bucket"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.func.arn
-#   principal     = "s3.amazonaws.com"
-#   source_arn    = aws_s3_bucket.bucket.arn
-# }
 
 
 # resource "aws_lambda_event_source_mapping" "bid_source" {
