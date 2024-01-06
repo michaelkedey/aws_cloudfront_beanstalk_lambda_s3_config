c60bf61b8638ab8c271b2a2cd61d3dc9cd8957ae
= <=>
2024-01-06 14:12:34 +0000

    lambda22222222

--- src/infrastracture/modules/lambda/lambda.tf
+++ src/infrastracture/modules/lambda/lambda.tf
@@ -84,14 +84,21 @@ resource "aws_lambda_function" "bid_lambda_fn" {
 }
 
 # # S3 event trigger for Lambda function
-resource "aws_s3_bucket_notification" "lambda_trigger" {
-  bucket = var.trigger_bucket_arn
+# resource "aws_s3_bucket_notification" "lambda_trigger" {
+#   bucket = var.trigger_bucket_arn
+#   lambda_function {
+#     lambda_function_arn = aws_lambda_function.bid_lambda_fn.arn
+#     events              = ["s3:ObjectCreated:*"]
+#     filter_prefix       = var.prefix
+#     filter_suffix       = var.suffix
+#   }
+# }
 
+resource "aws_s3_bucket_notification" "lambda_trigger" {
+  bucket = var.s3_bucket_bucket
   lambda_function {
     lambda_function_arn = aws_lambda_function.bid_lambda_fn.arn
     events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = var.prefix
-    filter_suffix       = var.suffix
   }
 }
 
