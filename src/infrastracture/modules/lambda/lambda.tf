9f2c0bf97f8c4ec48ccf5788d119f0445e5f67bd
= <=>
2024-01-06 13:18:01 +0000

    lambda

--- src/infrastracture/modules/lambda/lambda.tf
+++ src/infrastracture/modules/lambda/lambda.tf
@@ -88,7 +88,7 @@ resource "aws_s3_bucket_notification" "lambda_trigger" {
   bucket = var.s3_bucket_name
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.deploy_function.arn
+    lambda_function_arn = aws_lambda_function.bid_lambda_fn.arn
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = var.prefix
     filter_suffix       = var.suffix
