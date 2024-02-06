1be1a82c7e08ed3dfcefbc41d52534b84e2f7f63
michaelkedey <privatemyk@gmail.com>
2024-02-06 18:43:52 +0000

    clean

--- src/infrastructure/main.tf
+++ src/infrastructure/main.tf
@@ -10,12 +10,6 @@ module "bucket" {
   bucket_name = var.bucket_name
 }
 
-# #3 create bucket
-# module "bucket2" {
-#   source      = "./modules/s3"
-#   bucket_name = var.bucket2_name
-# }
-
 #3 archive lambda functions
 module "zip_lambda" {
   source      = "./modules/archive_file"
@@ -52,8 +46,6 @@ module "lambda" {
   s3_bucket_id         = module.bucket.bucket_id
   src_code_hash        = module.zip_lambda.src_code_hash
   eb_env_name          = module.beanstalk.environment_name
-  #trigger_bucket_arn = module.bucket.bucket_arn
-  #event_source_arn = module.primary_bucket.bucket_arn
 
 }
 
@@ -94,65 +86,4 @@ module "beanstalk" {
 #   beanstalk_elb_dns = module.beanstalk.c_name
 # }
 
-
-# ########################################################################
-
-# #9 trigger lambda
-# module "trigger_lambda_via_upload" {
-#   source       = "./modules/bucket_uploads_file"
-#   file_path    = var.app_file_trigger_upload
-#   s3_bucket_id = module.bucket.bucket_id
-#   key          = var.app_file_trigger_key
-# }
-
-#9 create dotnet app version
-# module "dotnet_app_version" {
-#   source           = "./modules/app_version"
-#   app_key          = var.app_key
-#   app_version_name = var.version_name
-#   bucket_id        = module.bucket.bucket_id
-#   application = module.dotnet_app.app_name
-
-# }
-
-#4 archive .net app directory
-# module "zip_dotnet" {
-#   source      = "./modules/archive_dir"
-#   src_path    = var.app_archive_source
-#   output_path = var.app_archive_output
-# }
-
-
-#11 deploy lb
-# module "load_balancer" {
-#   source = "./modules/loadbalancer"
-#   vpc_id = module.vpc.vpc_id
-#   subnet_for_lbs = [
-#     module.vpc.sn_public1_id,
-#     module.vpc.sn_public2_id
-#   ]
-#   lb_egress_cidrs = module.vpc.lb_out_cidrs
-#   beanstalk_sg    = module.vpc.beanstalk_sg_id
-#   #instance_ids    = module.beanstalk.instance_ids
-#   #lb_egress_cidrs = split(",",module.vpc.lb_out_cidrs)
-# }
-
-
-#12create cloudfront distribution
-# module "cf" {
-#   source             = "./modules/cloudfront"
-#   bucket_domain_name = module.primary_bucket.bucket_domain_name
-#   cf_origin_id       = module.primary_bucket.bucket_name
-# }
-
-
-#13 #create and attach cf bucket policy
-# module "cf_s3_policy" {
-#   source                      = "./modules/cf_bkt_policy"
-#   bucket_id_to_attache_policy = module.primary_bucket.bucket_id
-#   cloudfront_arn              = module.cf.cloudfront_arn
-#   bucket_arn                  = module.primary_bucket.bucket_arn
-# }
-
-##
 
